3cf58bfd53eebeb1b83a397fc6a4e3154c297402
jitendraneerukonda <jitendraneerukonda@gmail.com>
2023-01-11 14:40:15 -0600

    added hello world

---
+++ corejava/src/com/careerit/cg/day1/helloworlds.java
@@ -0,0 +1,7 @@
+package com.careerit.cg.day1;
+
+public class helloworlds {
+	public static void main(String[] args) {
+		System.out.println("Welcome");
+	}
+}
---
+++ corejava/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Jitendra Neerukonda
+ *
+ */
+module corejava {
+}
